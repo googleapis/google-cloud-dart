775a72377597ac4a29ee3d37b242570c859b8a3f
Brian Quinlan <bquinlan@google.com>
2025-10-10 18:29:00 -0700

    feat: Support the API for loading API keys from environment variables (#13)
    Code generation is added in
    https://github.com/googleapis/librarian/pull/2538

--- packages/google_cloud_gax/lib/gax.dart
+++ packages/google_cloud_gax/lib/gax.dart
@@ -17,6 +17,9 @@ import 'dart:convert';
 import 'package:google_cloud_gax/src/versions.dart';
 import 'package:google_cloud_rpc/rpc.dart';
 import 'package:http/http.dart' as http;
+export 'src/web.dart'
+    if (dart.library.io) 'src/vm.dart'
+    show environmentVariable;
 
 export 'dart:typed_data' show Uint8List;
 
