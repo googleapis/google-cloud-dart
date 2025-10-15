d82d594c70388c78c4df9ce7834bedaa4fb50ec4
Brian Quinlan <bquinlan@google.com>
2025-10-15 07:03:09 -0700

    Update main.dart

--- generated/google_cloud_ai_generativelanguage_v1beta/example/main.dart
+++ generated/google_cloud_ai_generativelanguage_v1beta/example/main.dart
@@ -15,7 +15,7 @@
 import 'package:google_cloud_ai_generativelanguage_v1beta/generativelanguage.dart';
 
 void main() async {
-  // XXX Pass your API key here if the GEMINI_API_KEY environment variable is not set.
+  // Pass your API key here if the GEMINI_API_KEY environment variable is not set.
   // See https://ai.google.dev/gemini-api/docs/api-key
   final service = GenerativeService.fromApiKey();
 
