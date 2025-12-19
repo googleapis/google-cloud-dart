a18ff0cfd824125d67b3221c7f8ee95ba65b8703
Brian Quinlan <bquinlan@google.com>
2025-12-19 10:09:25 -0800

    Update serialization_test.dart

--- generated/google_cloud_protojson_conformance/test/serialization_test.dart
+++ generated/google_cloud_protojson_conformance/test/serialization_test.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:google_cloud_protobuf_test_messages_proto3/google_cloud_protobuf_test_messages_proto3.dart';
 import 'package:test/test.dart';
 
@@ -13,73 +16,327 @@ void checkField(
   expect(feature(TestAllTypesProto3.fromJson(message.toJson())), matcher);
 }
 
-const minInt32 = -140737488355328;
-const maxInt32 = 140737488355327;
-const minInt64 = -9223372036854775808;
-const maxInt64 = 9223372036854775807;
-
 void main() async {
   group('test', () {
     group('int32', () {
-      test('min', () {
+      test('zero', () {
         checkField(
-          TestAllTypesProto3(optionalInt32: minInt32),
-          {'optionalInt32': minInt32},
+          TestAllTypesProto3(optionalInt32: 0),
+          {},
           (m) => m.optionalInt32,
-          minInt32,
+          0,
         );
       });
 
-      test('max', () {
+      test('non-zero', () {
         checkField(
-          TestAllTypesProto3(optionalInt32: maxInt32),
-          {'optionalInt32': maxInt32},
+          TestAllTypesProto3(optionalInt32: 1),
+          {'optionalInt32': 1},
           (m) => m.optionalInt32,
-          maxInt32,
+          1,
         );
       });
     });
 
     group('int64', () {
-      test('min', () {
+      test('zero', () {
         checkField(
-          TestAllTypesProto3(optionalInt64: minInt64),
-          {'optionalInt64': '-9223372036854775808'},
+          TestAllTypesProto3(optionalInt64: 0),
+          {},
           (m) => m.optionalInt64,
-          minInt64,
+          0,
         );
       });
 
-      test('max', () {
+      test('non-zero', () {
         checkField(
-          TestAllTypesProto3(optionalInt64: maxInt64),
-          {'optionalInt64': '9223372036854775807'},
+          TestAllTypesProto3(optionalInt64: 1),
+          {'optionalInt64': '1'},
           (m) => m.optionalInt64,
-          maxInt64,
+          1,
+        );
+      });
+    });
+
+    group('uint32', () {
+      test('zero', () {
+        checkField(
+          TestAllTypesProto3(optionalUint32: 0),
+          {},
+          (m) => m.optionalUint32,
+          0,
+        );
+      });
+
+      test('non-zero', () {
+        checkField(
+          TestAllTypesProto3(optionalUint32: 1),
+          {'optionalUint32': 1},
+          (m) => m.optionalUint32,
+          1,
+        );
+      });
+    });
+
+    group('uint64', () {
+      test('zero', () {
+        checkField(
+          TestAllTypesProto3(optionalUint64: BigInt.zero),
+          {},
+          (m) => m.optionalUint64,
+          BigInt.zero,
+        );
+      });
+
+      test('non-zero', () {
+        checkField(
+          TestAllTypesProto3(optionalUint64: BigInt.one),
+          {'optionalUint64': '1'},
+          (m) => m.optionalUint64,
+          BigInt.one,
+        );
+      });
+    });
+
+    group('sint32', () {
+      test('zero', () {
+        checkField(
+          TestAllTypesProto3(optionalSint32: 0),
+          {},
+          (m) => m.optionalSint32,
+          0,
+        );
+      });
+
+      test('non-zero', () {
+        checkField(
+          TestAllTypesProto3(optionalSint32: 1),
+          {'optionalSint32': 1},
+          (m) => m.optionalSint32,
+          1,
+        );
+      });
+    });
+
+    group('sint64', () {
+      test('zero', () {
+        checkField(
+          TestAllTypesProto3(optionalSint64: 0),
+          {},
+          (m) => m.optionalSint64,
+          0,
+        );
+      });
+
+      test('non-zero', () {
+        checkField(
+          TestAllTypesProto3(optionalSint64: 1),
+          {'optionalSint64': '1'},
+          (m) => m.optionalSint64,
+          1,
+        );
+      });
+    });
+
+    group('fixed32', () {
+      test('zero', () {
+        checkField(
+          TestAllTypesProto3(optionalFixed32: 0),
+          {},
+          (m) => m.optionalFixed32,
+          0,
+        );
+      });
+
+      test('non-zero', () {
+        checkField(
+          TestAllTypesProto3(optionalFixed32: 1),
+          {'optionalFixed32': 1},
+          (m) => m.optionalFixed32,
+          1,
+        );
+      });
+    });
+
+    group('fixed64', () {
+      test('zero', () {
+        checkField(
+          TestAllTypesProto3(optionalFixed64: BigInt.zero),
+          {},
+          (m) => m.optionalFixed64,
+          BigInt.zero,
+        );
+      });
+
+      test('non-zero', () {
+        checkField(
+          TestAllTypesProto3(optionalFixed64: BigInt.one),
+          {'optionalFixed64': '1'},
+          (m) => m.optionalFixed64,
+          BigInt.one,
+        );
+      });
+    });
+
+    group('sfixed32', () {
+      test('zero', () {
+        checkField(
+          TestAllTypesProto3(optionalSfixed32: 0),
+          {},
+          (m) => m.optionalSfixed32,
+          0,
+        );
+      });
+
+      test('non-zero', () {
+        checkField(
+          TestAllTypesProto3(optionalSfixed32: 1),
+          {'optionalSfixed32': 1},
+          (m) => m.optionalSfixed32,
+          1,
+        );
+      });
+    });
+
+    group('sfixed64', () {
+      test('zero', () {
+        checkField(
+          TestAllTypesProto3(optionalSfixed64: 0),
+          {},
+          (m) => m.optionalSfixed64,
+          0,
+        );
+      });
+
+      test('non-zero', () {
+        checkField(
+          TestAllTypesProto3(optionalSfixed64: 1),
+          {'optionalSfixed64': '1'},
+          (m) => m.optionalSfixed64,
+          1,
+        );
+      });
+    });
+
+    group('float', () {
+      test('zero', () {
+        checkField(
+          TestAllTypesProto3(optionalFloat: 0.0),
+          {},
+          (m) => m.optionalFloat,
+          0.0,
+        );
+      });
+
+      test('non-zero', () {
+        checkField(
+          TestAllTypesProto3(optionalFloat: 1.5),
+          {'optionalFloat': 1.5},
+          (m) => m.optionalFloat,
+          1.5,
+        );
+      });
+    });
+
+    group('double', () {
+      test('zero', () {
+        checkField(
+          TestAllTypesProto3(optionalDouble: 0.0),
+          {},
+          (m) => m.optionalDouble,
+          0.0,
+        );
+      });
+
+      test('non-zero', () {
+        checkField(
+          TestAllTypesProto3(optionalDouble: 1.5),
+          {'optionalDouble': 1.5},
+          (m) => m.optionalDouble,
+          1.5,
+        );
+      });
+    });
+
+    group('bool', () {
+      test('false', () {
+        checkField(
+          TestAllTypesProto3(optionalBool: false),
+          {},
+          (m) => m.optionalBool,
+          false,
+        );
+      });
+
+      test('true', () {
+        checkField(
+          TestAllTypesProto3(optionalBool: true),
+          {'optionalBool': true},
+          (m) => m.optionalBool,
+          true,
+        );
+      });
+    });
+
+    group('string', () {
+      test('empty', () {
+        checkField(
+          TestAllTypesProto3(optionalString: ''),
+          {},
+          (m) => m.optionalString,
+          '',
+        );
+      });
+
+      test('non-empty', () {
+        checkField(
+          TestAllTypesProto3(optionalString: 'foo'),
+          {'optionalString': 'foo'},
+          (m) => m.optionalString,
+          'foo',
+        );
+      });
+    });
+
+    group('bytes', () {
+      test('empty', () {
+        checkField(
+          TestAllTypesProto3(optionalBytes: Uint8List(0)),
+          {},
+          (m) => m.optionalBytes,
+          Uint8List(0),
+        );
+      });
+
+      test('non-empty', () {
+        final bytes = Uint8List.fromList([1]);
+        checkField(
+          TestAllTypesProto3(optionalBytes: bytes),
+          {'optionalBytes': 'AQ=='},
+          (m) => m.optionalBytes,
+          bytes,
         );
       });
     });
 
     group('map<int32, int32>', () {
-      test('min', () {
+      test('empty', () {
         checkField(
-          TestAllTypesProto3(mapInt32Int32: {minInt32: minInt32}),
-          {
-            'mapInt32Int32': {'-140737488355328': minInt32},
-          },
+          TestAllTypesProto3(mapInt32Int32: {}),
+          {},
           (m) => m.mapInt32Int32,
-          {minInt32: minInt32},
+          isEmpty,
         );
       });
 
-      test('max', () {
+      test('non-empty', () {
         checkField(
-          TestAllTypesProto3(mapInt32Int32: {maxInt32: maxInt32}),
+          TestAllTypesProto3(mapInt32Int32: {1: 5}),
           {
-            'mapInt32Int32': {'140737488355327': maxInt32},
+            'mapInt32Int32': {'1': 5},
           },
           (m) => m.mapInt32Int32,
-          {maxInt32: maxInt32},
+          {1: 5},
         );
       });
     });
