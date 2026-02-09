60b2d617aeb38f003a9b098f50ce4c8e92959b5f
Brian Quinlan <bquinlan@google.com>
2026-02-09 12:41:58 -0800

    Update packages/google_cloud_storage/test/test_utils.dart
    Co-authored-by: Nate Bosch <nbosch1@gmail.com>

--- packages/google_cloud_storage/test/test_utils.dart
+++ packages/google_cloud_storage/test/test_utils.dart
@@ -7,16 +7,16 @@ import 'package:test_utils/test_http_client.dart';
 const _bucketChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
 final _random = Random();
 
-String uniqueBucketName() => [
-  for (int i = 0; i < 60; i++)
+String _randomSuffix(int length) => [
+  for (int i = 0; i < length; i++)
     _bucketChars[_random.nextInt(_bucketChars.length)],
 ].join();
 
-String bucketName(String name) {
+String _bucketName(String name) {
   assert(name.length < 55, '"$name" is too long to append a random suffix.');
   return TestHttpClient.isRecording || TestHttpClient.isReplaying
       ? name
-      : '$name-${uniqueBucketName()}'.substring(0, 60);
+      : '$name-${_randomSuffix(59 - name.length)}';
 }
 
 String bucketNameWithTearDown(Storage storage, String name) {
