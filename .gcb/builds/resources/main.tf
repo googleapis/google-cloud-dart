f7cdf581e65e8470bfa15a3987a52e1a7e95b573
Brian Quinlan <bquinlan@google.com>
2026-03-10 13:46:12 -0700

    Fix

---
+++ .gcb/builds/resources/main.tf
@@ -0,0 +1,65 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+variable "project" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+# Create a bucket to cache build artifacts.
+resource "google_storage_bucket" "build-cache" {
+  name          = "${var.project}-build-cache"
+  force_destroy = false
+  # This prevents Terraform from deleting the bucket. Any plan to do so is
+  # rejected. If we really need to delete the bucket we must take additional
+  # steps.
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  # The bucket configuration.
+  location                    = "US-CENTRAL1"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+  versioning {
+    enabled = false
+  }
+  # Remove objects older than 90d. It is unlikely that any build artifact is
+  # usefull after that long, and we can always rebuild them if needed.
+  lifecycle_rule {
+    condition {
+      age = 90
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+resource "google_cloudbuild_worker_pool" "pool" {
+  name     = "dart-sdk-pool"
+  location = "us-central1"
+  worker_config {
+    disk_size_gb   = 100
+    machine_type   = "e2-standard-4"
+    no_external_ip = false
+  }
+}
+
+output "build-cache" {
+  value = resource.google_storage_bucket.build-cache.id
+}
---
+++ .gcb/builds/triggers/main.tf
@@ -0,0 +1,148 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+variable "project" {}
+variable "region" {}
+variable "service_account" {}
+
+locals {
+  gcb_app_installation_id = 1168573
+  gcb_secret_name = "projects/${var.project}/secrets/github-github-oauthtoken-319d75/versions/latest"
+
+  common_builds = {
+    integration = {
+      config = "integration.yaml"
+    }
+  }
+
+  pr_builds = local.common_builds
+  pm_builds = local.common_builds
+}
+
+data "google_project" "project" {
+}
+
+data "google_service_account" "terraform-runner" {
+  account_id = "terraform-runner"
+}
+
+resource "google_cloudbuildv2_connection" "github" {
+  project  = var.project
+  location = var.region
+  name     = "github"
+
+  github_config {
+    app_installation_id = local.gcb_app_installation_id
+    authorizer_credential {
+      oauth_token_secret_version = local.gcb_secret_name
+    }
+  }
+}
+
+resource "google_cloudbuildv2_repository" "main" {
+  project           = var.project
+  location          = var.region
+  name              = "googleapis-google-cloud-dart"
+  parent_connection = google_cloudbuildv2_connection.github.name
+  remote_uri        = "https://github.com/googleapis/google-cloud-dart.git"
+}
+
+resource "google_cloudbuild_trigger" "pull-request" {
+  for_each = tomap(local.pr_builds)
+  location = var.region
+  name     = "gcb-pr-${each.key}"
+  filename = ".gcb/${each.value.config}"
+  tags     = ["pull-request", "name:${each.key}"]
+
+  service_account = var.service_account
+
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.main.id
+    pull_request {
+      branch          = "^main$"
+      comment_control = "COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"
+    }
+  }
+
+  substitutions = {
+    _SCRIPT = lookup(each.value, "script", "")
+  }
+}
+
+resource "google_cloudbuild_trigger" "post-merge" {
+  for_each = {
+    for k, v in local.pm_builds : k => {
+      config         = v.config,
+      script         = try(v.script, "")
+      included_files = try(v.included_files, [])
+    }
+  }
+  location       = var.region
+  name           = "gcb-pm-${each.key}"
+  filename       = ".gcb/${each.value.config}"
+  tags           = ["post-merge", "push", "name:${each.key}"]
+  included_files = each.value.included_files
+
+  service_account = var.service_account
+
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.main.id
+    push {
+      branch = "^main$"
+    }
+  }
+
+  substitutions = {
+    _SCRIPT = lookup(each.value, "script", "")
+  }
+}
+
+resource "google_pubsub_topic" "terraform_runner_topic" {
+  name = "terraform-runner"
+}
+
+resource "google_pubsub_subscription" "terraform_runner_sub" {
+  name  = "terraform-sub"
+  topic = google_pubsub_topic.terraform_runner_topic.name
+}
+
+resource "google_cloud_scheduler_job" "job" {
+  name        = "terraform-job"
+  description = "Periodically sync terraform build"
+  schedule    = "0 0 * * 0" # Once a week at midnight on Sunday.
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.terraform_runner_topic.id
+    data       = base64encode("sync")
+  }
+}
+
+resource "google_cloudbuild_trigger" "pubsub-trigger" {
+  location = var.region
+  name     = "gcb-pubsub-terraform"
+  filename = ".gcb/terraform.yaml"
+  tags     = ["scheduler", "name:terraform"]
+
+  service_account = data.google_service_account.terraform-runner.id
+
+  pubsub_config {
+    topic = google_pubsub_topic.terraform_runner_topic.id
+  }
+
+  source_to_build {
+    repository = google_cloudbuildv2_repository.main.id
+    ref        = "refs/heads/main"
+    repo_type  = "GITHUB"
+  }
+}
