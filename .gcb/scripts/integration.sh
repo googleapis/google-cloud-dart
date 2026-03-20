a78000247fdbf0b16d0f589ea997427d9169a8cc
Brian Quinlan <bquinlan@google.com>
2026-03-20 13:32:34 -0700

    Update integration.sh

--- .gcb/scripts/integration.sh
+++ .gcb/scripts/integration.sh
@@ -14,7 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+# set -e
+set -o xtrace
 
 # Install docker if it is not already present.
 if ! command -v docker &> /dev/null
