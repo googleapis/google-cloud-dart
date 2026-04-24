42a6536ef46d550ad1261afbfe61503ebe5b9d49
Brian Quinlan <bquinlan@google.com>
2026-04-24 10:19:51 -0700

    feat(firestore): initial support (#245)
    Adds the configuration, tests, and example for a new generated package:
    `package:google_cloud_firestore_v1`
    
    Adds terraform configuration for firestore for integration tests.
    
    Before this change, only `package:google_cloud_storage` had tests that
    used the firebase emulator suite. The new firestore package adds
    additional emulator tests so:
    1. the emulator configuration is moved from
    `google_cloud_storage/emulator_test` to a top-level
    `firestore_emulators` directory.
    2. the emulator workflow is moved from
    `.github/workflows/gcs_emulator_tests.yaml` to `dart_checks.yaml` and is
    run against all tests (with the `"firebase_emulator"` tag) instead of
    just cloud storage tests.

---
+++ generated/google_cloud_firestore_v1/example/main.dart
@@ -0,0 +1,43 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:google_cloud_firestore_v1/firestore.dart';
+import 'package:googleapis_auth/auth_io.dart' as auth;
+
+void main() async {
+  // Connects to the Cloud Firestore API using Application Default
+  // Connections (ADC).
+  //
+  // Before running this example, you need to authenticate with gcloud:
+  //
+  // ```
+  // $ gcloud auth application-default login
+  // ```
+  //
+  // See https://cloud.google.com/docs/authentication/application-default-credentials
+  final client = await auth.clientViaApplicationDefaultCredentials(
+    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+  );
+  final service = Firestore(client: client);
+
+  final result = await service.createDocument(
+    CreateDocumentRequest(
+      parent: 'projects/<project id>/databases/<database id>/documents',
+      collectionId: 'users',
+      document: Document(fields: {'firstName': Value(stringValue: 'Brian')}),
+    ),
+  );
+  print('Created document: ${result.name}');
+  service.close();
+}
---
+++ generated/google_cloud_firestore_v1/test/firestore_test.dart
@@ -0,0 +1,114 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+@TestOn('vm')
+library;
+
+import 'package:google_cloud_firestore_v1/firestore.dart';
+import 'package:googleapis_auth/auth_io.dart' as auth;
+import 'package:http/http.dart' as http;
+import 'package:test/test.dart';
+import 'package:test_utils/cloud.dart';
+
+const databaseId = '(default)';
+
+void firestoreTest(Future<Firestore> Function() createFirestore) {
+  late Firestore firestoreService;
+
+  setUp(() async {
+    firestoreService = await createFirestore();
+  });
+
+  tearDown(() => firestoreService.close());
+
+  test('getDocument', () async {
+    final r = await firestoreService.createDocument(
+      CreateDocumentRequest(
+        parent: 'projects/$projectId/databases/$databaseId/documents',
+        collectionId: 'users',
+        document: Document(fields: {'firstName': Value(stringValue: 'Brian')}),
+      ),
+    );
+    addTearDown(
+      () =>
+          firestoreService.deleteDocument(DeleteDocumentRequest(name: r.name)),
+    );
+
+    final doc = await firestoreService.getDocument(
+      GetDocumentRequest(name: r.name),
+    );
+    expect(doc.fields['firstName']?.stringValue, 'Brian');
+  });
+
+  test('runQuery', () async {
+    final r = await firestoreService.createDocument(
+      CreateDocumentRequest(
+        parent: 'projects/$projectId/databases/$databaseId/documents',
+        collectionId: 'users',
+        document: Document(fields: {'firstName': Value(stringValue: 'Brian')}),
+      ),
+    );
+    addTearDown(
+      () =>
+          firestoreService.deleteDocument(DeleteDocumentRequest(name: r.name)),
+    );
+
+    final stream = firestoreService.runQuery(
+      RunQueryRequest(
+        parent: 'projects/$projectId/databases/$databaseId/documents',
+        structuredQuery: StructuredQuery(
+          from: [StructuredQuery_CollectionSelector(collectionId: 'users')],
+          where: StructuredQuery_Filter(
+            fieldFilter: StructuredQuery_FieldFilter(
+              field: StructuredQuery_FieldReference(fieldPath: 'firstName'),
+              op: StructuredQuery_FieldFilter_Operator.equal,
+              value: Value(stringValue: 'Brian'),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    final responses = await stream.toList();
+    expect(
+      responses,
+      contains(
+        isA<RunQueryResponse>().having((r) => r.document!.name, 'name', r.name),
+      ),
+    );
+  });
+}
+
+void main() {
+  group('firestore_v1', () {
+    group('google-cloud', tags: ['google-cloud'], () {
+      firestoreTest(
+        () async => Firestore(
+          client: await auth.clientViaApplicationDefaultCredentials(
+            scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+          ),
+        ),
+      );
+    });
+
+    group('firebase-emulator', tags: ['firebase-emulator'], () {
+      firestoreTest(
+        () async => Firestore(
+          client: http.Client(),
+          endPoint: Uri.http('127.0.0.1:8080'),
+        ),
+      );
+    });
+  });
+}
